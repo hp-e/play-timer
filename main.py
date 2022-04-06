8c959f6c235ca8146fb5203ef010ffc34913aa32
Hans-Petter Eitvet <hpephoto@gmail.com>
2022-04-06 16:30:47 +0200

    refactor: Major cleanup

--- main.py
+++ main.py
@@ -1,41 +1,89 @@
-# music.start_melody(music.built_in_melody(Melodies.BADDY),
-#         MelodyOptions.ONCE_IN_BACKGROUND)
-timer_started = 1
+elapsed = 0
+
+listY: List[number] = []
+listX: List[number] = []
+sound: List[number] = []
+
 timer_stopped = 0
+timer_started = 1
 timer_paused = 2
+
 timer_state = timer_stopped
-# music.set_volume()
-remaining_minutes = 0
 minute_step = 5
 minutes = 10
-listX: List[number] = []
-listY: List[number] = []
-sound: List[number] = []
-elapsed = 0
+remaining_minutes = minutes
 presses = 1
 initList()
 reset()
-
 sound = [131, 165, 196, 262, 294]
 
-def on_button_pressed_a():
-    global minutes, remaining_minutes, presses
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def initList():
+    for y in range(5):
+        for x in range(5):
+            listX.append(x)
+            listY.append(y)
+
+def reset():
+    basic.clear_screen()
+    set_default_values()
+    render_screen()
+
+def stop_timer():
+    global timer_state
+    timer_state = timer_stopped
+    basic.clear_screen()
+    for index in range(3):
+        led.fade_out(200)
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+        """)
+        led.fade_in(300)
+    reset()
+
+def on_button_pressed_a():
+    global presses, minutes, remaining_minutes
     if timer_state == timer_stopped:
         if presses == 4:
             presses = 0
             minutes = minute_step
-        else: 
-            presses = presses + 1            
+        else:
+            presses = presses + 1
             minutes = minutes + minute_step
-        
         remaining_minutes = minutes
         render_screen()
         music.play_tone(sound[presses], music.beat(BeatFraction.HALF))
-    
-input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_ab():    
+def pause_timer():    
+    global timer_state
+    timer_state = timer_paused
+    music.start_melody(music.built_in_melody(Melodies.FUNK),
+        MelodyOptions.ONCE_IN_BACKGROUND)
+
+def start_timer():
+    global timer_state
+    render_screen()
+    timer_state = timer_started
+    music.start_melody(music.built_in_melody(Melodies.POWER_UP),
+        MelodyOptions.ONCE_IN_BACKGROUND) 
+
+def on_timer_end():
+    global timer_state
+    timer_state = timer_stopped
+    music.start_melody(music.built_in_melody(Melodies.DADADADUM),
+        MelodyOptions.ONCE_IN_BACKGROUND)
+    for index in range(5):
+        led.fade_out(200)
+        basic.show_icon(IconNames.NO)
+        led.fade_in(300)
+    reset()
+
+def on_button_pressed_ab():
     if timer_state != timer_stopped:
         stop_timer()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
@@ -43,15 +91,29 @@ input.on_button_pressed(Button.AB, on_button_pressed_ab)
 def on_button_pressed_b():
     global timer_state
     led.fade_in()
-    if timer_state != timer_started:
-        render_screen()
-        timer_state = timer_started
-        music.start_melody(music.built_in_melody(Melodies.POWER_UP), MelodyOptions.ONCE_IN_BACKGROUND)
+    if timer_state != timer_started:    
+        start_timer()            
     else:
-        timer_state = timer_paused
-        music.start_melody(music.built_in_melody(Melodies.FUNK), MelodyOptions.ONCE_IN_BACKGROUND)
-
+        pause_timer()        
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def set_default_values():
+    global timer_state, remaining_minutes
+    timer_state = timer_stopped
+    remaining_minutes = minutes
+
+def render_screen():
+    basic.clear_screen()
+    x = 0
+    y = 0
+    minIdx = remaining_minutes - 1
+    for index in range(remaining_minutes):
+        x = listX[index]
+        y = listY[index]
+        led.plot(x, y)
+
+
+
 
 def on_every_interval():
     global elapsed, remaining_minutes, timer_state
@@ -68,68 +130,14 @@ def on_every_interval():
         if remaining_minutes <= 0:
             timer_state = timer_stopped
             on_timer_end()
-    if timer_state == timer_paused:        
-        pause_timer()
-loops.every_interval(1000, on_every_interval)
-
-def on_timer_end():
-    global timer_state
-    timer_state = timer_stopped
-    
-    music.start_melody(music.built_in_melody(Melodies.DADADADUM),
-            MelodyOptions.ONCE_IN_BACKGROUND)
-        
-    for x2 in range(5):
-        led.fade_out(200)
-        basic.show_icon(IconNames.NO)        
-        led.fade_in(300)
-
-    reset()
-def stop_timer():
-    global timer_state
-    timer_state = timer_stopped
-    basic.clear_screen()
-    for x3 in range(3):
-        led.fade_out(200)
+    if timer_state == timer_paused:
+        led.fade_out(100)
         basic.show_leds("""
             . . . . .
-            . # # # .
             . # . # .
-            . # # # .
+            . # . # .
+            . # . # .
             . . . . .
         """)
-        # basic.pause(500)
         led.fade_in(300)
-    reset()
-def pause_timer():
-    led.fade_out(100)                    
-    basic.show_leds("""
-        . . . . .
-        . # . # .
-        . # . # .
-        . # . # .
-        . . . . .
-    """)
-    led.fade_in(300)
-def set_default_values():
-    global timer_state, remaining_minutes
-    timer_state = timer_stopped
-    remaining_minutes = minutes
-def render_screen():
-    basic.clear_screen()
-    x4 = 0
-    y2 = 0
-    minIdx = remaining_minutes - 1
-    for index in range(remaining_minutes):
-        x4 = listX[index]
-        y2 = listY[index]
-        led.plot(x4, y2)
-def reset():
-    basic.clear_screen()
-    set_default_values()
-    render_screen()
-def initList():
-    for y3 in range(5):
-        for x5 in range(5):
-            listX.append(x5)
-            listY.append(y3)
+loops.every_interval(1000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
-//  music.start_melody(music.built_in_melody(Melodies.BADDY),
-//          MelodyOptions.ONCE_IN_BACKGROUND)
-let timer_started = 1
+let elapsed = 0
+let listY : number[] = []
+let listX : number[] = []
+let sound : number[] = []
 let timer_stopped = 0
+let timer_started = 1
 let timer_paused = 2
 let timer_state = timer_stopped
-//  music.set_volume()
-let remaining_minutes = 0
 let minute_step = 5
 let minutes = 10
-let listX : number[] = []
-let listY : number[] = []
-let sound : number[] = []
-let elapsed = 0
+let remaining_minutes = minutes
 let presses = 1
 initList()
 reset()
@@ -33,6 +30,64 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     }
     
 })
+function initList() {
+    for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < 5; x++) {
+            listX.push(x)
+            listY.push(y)
+        }
+    }
+}
+
+function reset() {
+    basic.clearScreen()
+    set_default_values()
+    render_screen()
+}
+
+function stop_timer() {
+    
+    timer_state = timer_stopped
+    basic.clearScreen()
+    for (let index = 0; index < 3; index++) {
+        led.fadeOut(200)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+        `)
+        led.fadeIn(300)
+    }
+    reset()
+}
+
+function pause_timer() {
+    
+    timer_state = timer_paused
+    music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.OnceInBackground)
+}
+
+function start_timer() {
+    
+    render_screen()
+    timer_state = timer_started
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
+}
+
+function on_timer_end() {
+    
+    timer_state = timer_stopped
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.OnceInBackground)
+    for (let index = 0; index < 5; index++) {
+        led.fadeOut(200)
+        basic.showIcon(IconNames.No)
+        led.fadeIn(300)
+    }
+    reset()
+}
+
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     if (timer_state != timer_stopped) {
         stop_timer()
@@ -43,15 +98,30 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
     led.fadeIn()
     if (timer_state != timer_started) {
-        render_screen()
-        timer_state = timer_started
-        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
+        start_timer()
     } else {
-        timer_state = timer_paused
-        music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.OnceInBackground)
+        pause_timer()
     }
     
 })
+function set_default_values() {
+    
+    timer_state = timer_stopped
+    remaining_minutes = minutes
+}
+
+function render_screen() {
+    basic.clearScreen()
+    let x = 0
+    let y = 0
+    let minIdx = remaining_minutes - 1
+    for (let index = 0; index < remaining_minutes; index++) {
+        x = listX[index]
+        y = listY[index]
+        led.plot(x, y)
+    }
+}
+
 loops.everyInterval(1000, function on_every_interval() {
     let x: number;
     let y: number;
@@ -76,83 +146,15 @@ loops.everyInterval(1000, function on_every_interval() {
     }
     
     if (timer_state == timer_paused) {
-        pause_timer()
+        led.fadeOut(100)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+        `)
+        led.fadeIn(300)
     }
     
 })
-function on_timer_end() {
-    
-    timer_state = timer_stopped
-    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.OnceInBackground)
-    for (let x2 = 0; x2 < 5; x2++) {
-        led.fadeOut(200)
-        basic.showIcon(IconNames.No)
-        led.fadeIn(300)
-    }
-    reset()
-}
-
-function stop_timer() {
-    
-    timer_state = timer_stopped
-    basic.clearScreen()
-    for (let x3 = 0; x3 < 3; x3++) {
-        led.fadeOut(200)
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
-            . . . . .
-        `)
-        //  basic.pause(500)
-        led.fadeIn(300)
-    }
-    reset()
-}
-
-function pause_timer() {
-    led.fadeOut(100)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . # . # .
-        . # . # .
-        . . . . .
-    `)
-    led.fadeIn(300)
-}
-
-function set_default_values() {
-    
-    timer_state = timer_stopped
-    remaining_minutes = minutes
-}
-
-function render_screen() {
-    basic.clearScreen()
-    let x4 = 0
-    let y2 = 0
-    let minIdx = remaining_minutes - 1
-    for (let index = 0; index < remaining_minutes; index++) {
-        x4 = listX[index]
-        y2 = listY[index]
-        led.plot(x4, y2)
-    }
-}
-
-function reset() {
-    basic.clearScreen()
-    set_default_values()
-    render_screen()
-}
-
-function initList() {
-    for (let y3 = 0; y3 < 5; y3++) {
-        for (let x5 = 0; x5 < 5; x5++) {
-            listX.push(x5)
-            listY.push(y3)
-        }
-    }
-}
-
